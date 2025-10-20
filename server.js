50b770e4fc6ca4d8d05e5339d07e78830a39ecb3
gitcat456 <okothdenzel65@gmail.com>
2025-10-20 11:27:18 +0300

    configures server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port:${PORT}`);
+});
